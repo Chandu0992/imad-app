744a2db0bdee37633bfad0c94ffff5916be5f546
Chandu0992 <30655263+Chandu0992@users.noreply.github.com>
2017-08-22 08:34:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 function hash(input,salt){
-	var hashed = crypto.createHmac(input,salt,10000,512,'sha512');
+	var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
 	return hashed.toString('hex');
 }
 app.get('/hash/:input',function(req,res){
